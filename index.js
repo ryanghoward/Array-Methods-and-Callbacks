6dbfb3f88a29936bd2db069cdce2c416b02e04c1
Ryan Howard <ryanghoward@protonmail.com>
2021-09-14 10:58:38 -0700

    Task 1 done

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const { fifaData } = require('./fifa.js')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-fifaData.filter(function(item){
-    return item.Year === 2014;
-})
+const finals2014 = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final';
+});
+
+console.log(finals2014);
+
 //(a) Home Team name for 2014 world cup final
-
+console.log(finals2014[0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
-
+console.log(finals2014[0]['Away Team Name']);
 //(c) Home Team goals for 2014 world cup final
-
+console.log(finals2014[0]['Home Team Goals']);
 //(d) Away Team goals for 2014 world cup final
-
+console.log(finals2014[0]['Away Team Goals']);
 //(e) Winner of 2014 world cup final */
-
+console.log(finals2014[0]['Win conditions']);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -51,6 +54,7 @@ Use the higher-order function getWinners to do the following:
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */ 
 
+//Don't worry about overtime - compare home team goals and away team goals
 function getWinners(/* code here */) {
     /* code here */
 }
@@ -65,7 +69,7 @@ Use the higher-order function getWinnersByYear to do the following:
 4. Return an array of strings that say "In {year}, {country} won the world cup!" 
 
 hint: the strings returned need to exactly match the string in step 4.
- */
+ Use map with item to grab year and index to grab winner*/
 
 function getWinnersByYear(/* code here */) {
     /* code here */
@@ -81,9 +85,9 @@ Use the higher order function getAverageGoals to do the following:
  (Hint: use .reduce and do this in 2 steps) 
  
  Example of invocation: getAverageGoals(getFinals(fifaData));
-*/
+use toFixed(2)*/
 
-function getAverageGoals(/* code here */) {
+function getAverageGoals(/* code here - only one param*/) {
    /* code here */
 }
 
